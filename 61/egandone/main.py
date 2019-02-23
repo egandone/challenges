c0dbcfe225d240cf6ffa77ea062b56dad4b39409
Egan Done <edone@sympatico.ca>
2019-02-23 17:49:24 -0500

    Moved home page to Jinja template

--- 61/egandone/main.py
+++ 61/egandone/main.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>PyBites Blog Code Challenge 61 - URL shortener</h1>'
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
